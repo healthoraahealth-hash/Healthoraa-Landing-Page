1e9ad8aac63a227ed64ffa3c43c023839af03bb3
unknown <Mandeep-dk>
2025-12-16 18:15:55 +0530

    Form submission test

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
+
 const formAPI = axios.create({ baseURL: `${BASE_URL}/api/formSubmission` });
 
 export const submitForm = (data) => formAPI.post(`/`, data);
